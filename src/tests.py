8c9ba39011eb905f82925f8cd67c8d23f31ffc7e
Erick <hearteric57@gmail.com>
2023-03-12 21:55:47 +0300

    Added testcase to check basic markdown to html conversion

--- src/tests.py
+++ src/tests.py
@@ -13,16 +13,24 @@ from lazarus.engine import Engine
 class TestLazarusEngine(unittest.TestCase):
     '''Tests the lazarus engine'''
 
-    def test_formatting_string(self):
+    def setUp(self):
+        '''
+        Create a lazarus engine instance that is to be used 
+        in the test framework
+        '''
+
+        self.lazarus = Engine('Lazarus Test')
+
+    def test_basic_markdown_conversion(self):
         '''
         Tests if the Lazarus engine can perform basic conversion
         of a markdown string to a html string
         '''
 
         markdown_string = '#Lazarus Engine!'
-        lazarus = Engine(True)
+        self.lazarus.convert(markdown_string)
         
-        self.assertEqual('<h1>Lazarus Engine!</h1>', lazarus.convert(markdown_string),
+        self.assertEqual('<h1>Lazarus Engine!</h1>', self.lazarus.convert(markdown_string),
                          'Couldn\'t Convert from markdown to HTML')
 
 
